8337b5160f3562194d0f548e7edb8f7c053cb8c1
AmirHossein Zare <amirhzare@users.noreply.github.com>
2024-04-24 11:53:32 +0330

    feat: able to delete a room function

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
-
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
 ]
